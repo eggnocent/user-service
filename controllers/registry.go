06e95eb054250062240b5f394c1722db87c8a860
eggnocent <egiwiratamaaa@gmail.com>
2025-08-18 11:03:17 +0700

    finishing controller

--- controllers/registry.go
+++ controllers/registry.go
@@ -1,1 +1,22 @@
 package controllers
+
+import (
+	controller "user-service/controllers/user"
+	"user-service/services"
+)
+
+type Registry struct {
+	service services.IServiceRegistry
+}
+
+type IControllerRegistry interface {
+	GetUserController() controller.IUserController
+}
+
+func NewControllerRegistry(service services.IServiceRegistry) IControllerRegistry {
+	return &Registry{service: service}
+}
+
+func (u *Registry) GetUserController() controller.IUserController {
+	return controller.NewUserController(u.service)
+}
--- controllers/user/user.go
+++ controllers/user/user.go
@@ -18,7 +18,7 @@ type IUserController interface {
 	Login(*gin.Context)
 	Register(*gin.Context)
 	Update(*gin.Context)
-	GetUserLgin(*gin.Context)
+	GetUserLogin(*gin.Context)
 	GetUserByUUID(*gin.Context)
 }
 
@@ -101,6 +101,88 @@ func (u *UserController) Register(ctx *gin.Context) {
 	}
 
 	user, err := u.service.GetUser().Register(ctx, request)
+	if err != nil {
+		response.HttpResponse(response.ParamHTTPResp{
+			Code: http.StatusBadRequest,
+			Err:  err,
+			Gin:  ctx,
+		})
+		return
+	}
+
+	response.HttpResponse(response.ParamHTTPResp{
+		Code: http.StatusOK,
+		Data: user.User,
+		Gin:  ctx,
+	})
+}
+
+func (u *UserController) Update(ctx *gin.Context) {
+	request := &dto.UpdateRequest{}
+	uuid := ctx.Param("uuid")
+
+	err := ctx.ShouldBindJSON(request)
+	if err != nil {
+		response.HttpResponse(response.ParamHTTPResp{
+			Code: http.StatusBadRequest,
+			Err:  err,
+			Gin:  ctx,
+		})
+		return
+	}
+
+	validate := validator.New()
+	err = validate.Struct(request)
+	if err != nil {
+		errMessage := http.StatusText(http.StatusUnprocessableEntity)
+		errResponse := errWrap.ErrValidationResponse(err)
+		response.HttpResponse(response.ParamHTTPResp{
+			Code:    http.StatusUnprocessableEntity,
+			Message: &errMessage,
+			Data:    errResponse,
+			Err:     err,
+			Gin:     ctx,
+		})
+		return
+	}
+
+	user, err := u.service.GetUser().Update(ctx, request, uuid)
+	if err != nil {
+		response.HttpResponse(response.ParamHTTPResp{
+			Code: http.StatusBadRequest,
+			Err:  err,
+			Gin:  ctx,
+		})
+		return
+	}
+
+	response.HttpResponse(response.ParamHTTPResp{
+		Code: http.StatusOK,
+		Data: user,
+		Gin:  ctx,
+	})
+}
+
+func (u *UserController) GetUserLogin(ctx *gin.Context) {
+	user, err := u.service.GetUser().GetUserLogin(ctx.Request.Context())
+	if err != nil {
+		response.HttpResponse(response.ParamHTTPResp{
+			Code: http.StatusBadRequest,
+			Err:  err,
+			Gin:  ctx,
+		})
+		return
+	}
+
+	response.HttpResponse(response.ParamHTTPResp{
+		Code: http.StatusOK,
+		Data: user,
+		Gin:  ctx,
+	})
+}
+
+func (u *UserController) GetUserByUUID(ctx *gin.Context) {
+	user, err := u.service.GetUser().GetUserByUUD(ctx.Request.Context(), ctx.Param("uuid"))
 	if err != nil {
 		response.HttpResponse(response.ParamHTTPResp{
 			Code: http.StatusBadRequest,
